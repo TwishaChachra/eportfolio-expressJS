cb1b0f5e3d37dfe39d91ddc77565e9552ec24cb7
Twisha Chachra <twishachachra@Twishas-MacBook-Air.local>
2022-10-11 22:08:25 -0400

    new commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>Personal Portfolio</h1>
+<p>It contains information about me.</p>
+<p>It contains a list of courses that I have learned till now.</p>
+<p>It contains my email id is someone wants to contact me</p>
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'ePortfolio' });
 });
 
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About Me' });
+});
+
+router.get('/project', (req, res, next) => {
+  res.render('project', { title: 'Projects' });
+});
+
+router.get('/contact', (req, res, next) => {
+  res.render('contact', { title: 'Contact Me' });
+});
 module.exports = router;
